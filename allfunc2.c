e6b547fd3e52c6413dc47592b44bd44e34dafb16
githinjialberet <githinjialbert001@gmail.com>
2023-05-19 06:36:00 -0700

    lineread.c

--- allfunc2.c
+++ allfunc2.c
@@ -24,3 +24,80 @@ char *customStrdup(char *str)
 	customStrcpy(ano_new, str);
 	return (ano_new);
 }
+/**
+*freeTokens - it frees tokens
+*@tokens: the tokens to be freed
+*/
+void freeTokens(char **tokens)
+{
+	int k;
+
+	if (tokens == NULL)
+	{
+		return;
+	}
+	k = 0;
+	for (k = 0; tokens[k] != NULL; k++)
+	{
+		free(tokens[k]);
+	}
+	free(tokens);
+}
+/**
+*My_customStrchr - in the input string it finds c
+*@s: the string to find c
+*@c: the char to be found
+*Return: the char found
+*/
+char *My_customStrchr(const char *s, int c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return ((char *)s);
+		}
+		s++;
+	}
+	return (NULL);
+}
+/**
+*customStrncpy - copies a string up to a no of btytes
+*@src: string to be copied
+*@dest: the destination of the string to be copied
+*@n: the number of bytes copied
+*Return: a pointer to the destination
+*/
+char *customStrncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
+/**
+*customStrchr - it locates a character in a string
+*@s: the string containing the character
+*@c: the character to be found
+*Return: a character
+*/
+char *customStrchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (c == '\0')
+	{
+		return (s);
+	}
+	return (NULL);
+}
---
+++ lineread.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+*displayPrompt - displays the prompt for the user to
+*key commands
+*Return: void
+*/
+void displayPrompt(void)
+{
+	char *prompt = "$ ";
+
+	write(1, prompt, customStrlen(prompt));
+}
+/**
+*readLine - reads the input keyed by the user in prompt
+*Return: void
+*/
+char *readLine(void)
+{
+	char *new_command;
+	size_t v;
+	ssize_t read;
+	int id;
+
+	new_command = NULL;
+	v = 0;
+	read = customGetline(&new_command, &v, stdin);
+	if (read == -1)
+	{
+		perror("getline failed");
+		exit(1);
+	}
+	id = read - 1;
+	if (new_command[id] = '\n')
+	{
+		new_command[id] = '\0';
+	}
+	return (new_command);
+}
