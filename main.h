680a6a603a77faa2778e3daad64917b7942788c9
githinjialberet <githinjialbert001@gmail.com>
2023-05-19 00:09:46 -0700

    simc.c

--- main.h
+++ main.h
@@ -19,9 +19,55 @@ typedef struct node
 	struct node *next;
 }Node;
 
+typedef struct 
+{
+	char *last_token;
+}tokenise;
 
 
+#define TOKEN_BUFSIZE 1024
+#define BUF_SIZE 1024
+#define TOKEN_SEPERATOR "\t\r\n\a,;:"
 
+extern char **environ;
+/**
+ *its a global variable
+ */
+
+void print_prompt(void);
+char *read_line(void);
+char **parse_line(char *new_command);
+char *find_executable(char **tokens);
+int child_process(char *command_path, char **tokens);
+void handle_env(char **tokens);
+void handle_exit(char **tokens);
+void exit_handler(char **arg);
+char *_getenv(char *name);
+Node *path_LL;
+char *handle_seperators(char *new_command);
+int change_directory(char **args);
+ssize_t _getline(char **new_command, size_t *n, FILE *stream);
+int _setenev(char *name, char *value, int overwrite);
+int _unsetenev(char *name);
+void operand(char *new_command);
+char* _strtok(tokenise *str, char* string, const char* delimiter);
+char *my_strchr(const char *str, int c);
+
+
+/* void *_realloc(void *oldPtr, int size) */
+int _strcmp(char *s1, char *s2);
+char *_strchr(char *s, char c);
+char *_strdup(char *str);
+int _atoi(char *str);
+char *_memcpy(char *dest, char *src, size_t size);
+
+void *_realloc(void *oldPtr, int oldSize, int size);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+char *_strncpy(char *dest, char *src, int n);
+int _strlen(char *s);
+void free_tokens(char **tokens);
+int _strncmp(char *s1, char *s2, int n);
 
 
 
--- simc.c
+++ simc.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
-int main(__attirbute__((unused))int argc, char **argv)
+int main(__attribute__((unused))int argc, char **argv)
 {
 	int i;
-	char *new_command, new_command_path;
+	char *new_command, *command_path;
 	size_t bufsize;
 	char **new_commands;
 
@@ -18,43 +18,43 @@ int main(__attirbute__((unused))int argc, char **argv)
 	{
 		if (isatty(STDIN_FILENO))
 		{
-			display_prompt();
+			print_prompt();
 			fflush(stdout);
-			new_command = read_input();
+			new_command = read_line();
 			if (feof(stdin))
 			{
 				write(STDOUT_FILENO, "\n", 1);
 				break;
 			}
-			new_commands = seperate_commands(new_command);
+			new_commands = handle_seperators(new_command);
 			for (i = 0; new_commands[i] != NULL; i++)
 			{
-				argv = parse_input(new_commands[i]);
+				argv = parse_line(new_commands[i]);
 				if (_strcmp(argv[0], "exit") == 0)
 				{
 					if (argv[1] != NULL)
 					{
-						exit_input_handler(argv);
+						exit_handler(argv);
 					}
 					else
 					{
-						handle_exit_input(argv);
+						handle_exit(argv);
 					}
 				}
 				else if (_strcmp(argv[0], "env") == 0)
 				{
-					handle_env_input(argv);
+					handle_env(argv);
 				}
 				else if (_strcmp(argv[0], "cd") == 0)
 				{
-					change_dir_input(argv);
+					change_directory(argv);
 				}
 				else
 				{
-					new_command_path = get_executable(argv);
-					child_process_input(new_command_path, argv);
+					command_path = find_executable(argv);
+					child_process(command_path, argv);
 				}
-				free(new_command_path);
+				free(command_path);
 			}
 			free_tokens(new_commands);
 		}
